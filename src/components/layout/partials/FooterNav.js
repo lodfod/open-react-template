4d58b384fa18555286aa11808a26e30550534e2f
archish-edu <archisharun99@gmail.com>
2021-12-12 17:41:23 -0500

    update footerNav.js

--- src/components/layout/partials/FooterNav.js
+++ src/components/layout/partials/FooterNav.js
@@ -19,13 +19,13 @@ const FooterNav = ({
     >
       <ul className="list-reset">
         <li>
-          <Link to="#0">Contact</Link>
+          <Link to="/contact">Contact</Link>
         </li>
         <li>
           <Link to="#0">About us</Link>
         </li>
         <li>
-          <Link to="#0">FAQs</Link>
+          <Link to="/contact">FAQs</Link>
         </li>
       </ul>
     </nav>
