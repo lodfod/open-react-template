c717f4a8901a01753696a0a9d545d0d0f8d876f3
archish-edu <archisharun99@gmail.com>
2022-02-09 18:51:16 -0500

    Updated UI Splash

--- src/components/sections/Cta.js
+++ src/components/sections/Cta.js
@@ -54,7 +54,7 @@ const Cta = ({
         >
           <div className="cta-slogan">
             <h3 className="m-0">
-              Join For Updates
+              Follow For Updates!
               </h3>
           </div>
           <div className="cta-action">
